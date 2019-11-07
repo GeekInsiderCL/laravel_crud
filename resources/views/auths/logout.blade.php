dd9ec236d11b727681ca5ce0edf4f08efc86d1ff
Ary Syaddam <ary.syaddam@gmail.com>
2019-11-07 13:00:29 +0700

    Fixing Login & Logout Appearance

--- resources/views/auths/logout.blade.php
+++ resources/views/auths/logout.blade.php
@@ -9,7 +9,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Dashboard - Login</title>
+  <title>Dashboard - Logout</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -28,34 +28,23 @@
     <div class="row justify-content-center">
     
       <div class="col-xl-5 col-lg-12 col-md-9">
-      <br><br>
+        <br><br>
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0 ">
             <!-- Nested Row within Card Body -->
             <div class="row">
               <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
               <div class="col-12">
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 mb-4">Welcome !</h1>
+                <div class="p-5">                  
+                  <div class="alert alert-success text-center" role="alert">
+                    <h4 class="h4"><a class="btn btn-success btn-circle btn-lg">
+                    <i class="fas fa-check"></i>
+                  </a><br><br>You have been successfully logged out.</h4>
                   </div>
-                  <form class="user" action="/postlogin" method="POST">
-                  {{ csrf_field() }}
-                    <hr>
-                    <div class="form-group">
-                      <input name="email" type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
-                    </div>
-                    <div class="form-group">
-                      <input name="password" type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
-                    </div>
-                    <hr>
-                    <button type="submit" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </button>
-                    
-                    
-                  </form>
-                  
+                  <br>
+                    <a href="/login" type="submit" class="btn btn-primary btn-user btn-block">
+                      Click here to continue...
+                    </a>
                 </div>
                 @include('layouts.includes._footer')
               </div>
