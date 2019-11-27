24d559dc6ae66f64f444759deeb8b42fc0dd6b07
Ary Syaddam <ary.syaddam@gmail.com>
2019-11-27 20:27:10 +0700

    change logout display.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -65,7 +65,7 @@
 		<div class="modal-dialog" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-			<h5 class="modal-title" id="exampleModalLabel">Do You Want To Logout ?</h5>
+			<h5 class="modal-title" id="exampleModalLabel">Do you want to logout ?</h5>
 			<button class="close" type="button" data-dismiss="modal" aria-label="Close">
 				<span aria-hidden="true">×</span>
 			</button>
